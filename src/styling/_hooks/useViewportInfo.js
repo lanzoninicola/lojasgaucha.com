bb6202f7be08ff1814e10a5be9c4e2adeac51dad
Nicola <lanzoni.nicola@gmail.com>
2021-01-30 09:24:41 -0300

    solving build problems with react-leaflet and server side rendering

--- src/styling/_hooks/useViewportInfo.js
+++ src/styling/_hooks/useViewportInfo.js
@@ -1,53 +1,58 @@
 import * as React from "react"
 import { debounce } from "../layouts/utils/functions"
 import { ThemeContext } from "styled-components"
+import { isDomAvailable } from "@utils/index"
 
 function useViewportInfo() {
-  const [viewportInfo, setviewportInfo] = React.useState({
-    device: "mobile",
-    height: window.innerHeight,
-    width: window.innerWidth,
-  })
+  const [viewportInfo, setviewportInfo] = React.useState()
 
   const themeContext = React.useContext(ThemeContext)
 
   React.useEffect(() => {
-    const debouncedHandleResize = debounce(function handleResize() {
-      let device = "laptop"
-
-      const breakpoints = themeContext?.breakpoints
-
-      // console.log(window.innerWidth)
-
-      if (window.innerWidth >= breakpoints?.small?.width) {
-        device = breakpoints?.small?.device
-      }
-
-      if (window.innerWidth >= breakpoints?.medium?.width) {
-        device = breakpoints?.medium?.device
-      }
-
-      if (window.innerWidth >= breakpoints?.large?.width) {
-        device = breakpoints?.large?.device
-      }
-
-      if (window.innerWidth >= breakpoints?.xlarge?.width) {
-        device = breakpoints?.xlarge?.device
-      }
-
-      if (window.innerWidth >= breakpoints?.xxlarge?.width) {
-        device = breakpoints?.xxlarge?.device
-      }
-
+    if (isDomAvailable) {
       setviewportInfo({
-        device: device,
+        device: "mobile",
         height: window.innerHeight,
         width: window.innerWidth,
       })
-    }, 200)
-    window.addEventListener("resize", debouncedHandleResize)
-    return _ => {
-      window.removeEventListener("resize", debouncedHandleResize)
+
+      const debouncedHandleResize = debounce(function handleResize() {
+        let device = "laptop"
+
+        const breakpoints = themeContext?.breakpoints
+
+        // console.log(window.innerWidth)
+
+        if (window.innerWidth >= breakpoints?.small?.width) {
+          device = breakpoints?.small?.device
+        }
+
+        if (window.innerWidth >= breakpoints?.medium?.width) {
+          device = breakpoints?.medium?.device
+        }
+
+        if (window.innerWidth >= breakpoints?.large?.width) {
+          device = breakpoints?.large?.device
+        }
+
+        if (window.innerWidth >= breakpoints?.xlarge?.width) {
+          device = breakpoints?.xlarge?.device
+        }
+
+        if (window.innerWidth >= breakpoints?.xxlarge?.width) {
+          device = breakpoints?.xxlarge?.device
+        }
+
+        setviewportInfo({
+          device: device,
+          height: window.innerHeight,
+          width: window.innerWidth,
+        })
+      }, 200)
+      window.addEventListener("resize", debouncedHandleResize)
+      return _ => {
+        window.removeEventListener("resize", debouncedHandleResize)
+      }
     }
   })
 
