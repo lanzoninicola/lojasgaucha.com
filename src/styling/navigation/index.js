e26f499c0642ca4d4580cb9bfeff6bd4c5ee690a
Nicola <lanzoni.nicola@gmail.com>
2021-02-12 14:02:19 -0300

    solve spelling error in navigation module

--- src/styling/navigation/index.js
+++ src/styling/navigation/index.js
@@ -2,6 +2,6 @@ import NavLink from "./navLink"
 import NavDiv from "./navDiv"
 import NavAnchor from "./navAnchor"
 import NavModal from "./navModal"
-import NavigationWrapper from "./NavigationWrapper"
+import NavigationWrapper from "./navigationWrapper"
 
 export { NavLink, NavDiv, NavAnchor, NavModal, NavigationWrapper }
